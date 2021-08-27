9d4d6807fddade241b5d346becb5f9ee7467147e
dhanaraj <dhanaraj@propelsoft>
2021-08-27 12:22:02 +0530

    Rename Password text Symbol

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -9,14 +9,12 @@ import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 import 'package:propel/auth/ForgotPassword/forgot_password.dart';
 import 'package:propel/auth/Person/distnict_data.dart';
 
-
-class LoginPage  extends StatefulWidget {
+class LoginPage extends StatefulWidget {
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage > {
-
+class _LoginPageState extends State<LoginPage> {
   final phoneNo = new TextEditingController();
   bool phoneVal = false;
   bool invalidphone = false;
@@ -25,8 +23,7 @@ class _LoginPageState extends State<LoginPage > {
   String initialCountry = 'IN';
   PhoneNumber number = PhoneNumber(isoCode: 'IN');
   @override
-  void _mobile_no_check(mobile_no) async{
-
+  void _mobile_no_check(mobile_no) async {
     bl = new BottomLoader(
       context,
       showLogs: true,
@@ -37,94 +34,82 @@ class _LoginPageState extends State<LoginPage > {
     );
     bl.display();
 
-    var res = await Network().Mobile_NO_Check('/getPersonByMobileNo/$mobile_no');
+    var res =
+        await Network().Mobile_NO_Check('/getPersonByMobileNo/$mobile_no');
     var body = json.decode(res.body);
 
-
-
-    if(body['message'] == "SUCCESS"){
-
-
-      if(body['data']['pId'] != ''){
-
-
-        if(body['data']['pHavingUser']){
-
+    if (body['message'] == "SUCCESS") {
+      if (body['data']['pId'] != '') {
+        if (body['data']['pHavingUser']) {
           var Name = body['data']['pFirstName'] + body['data']['pMiddleName'];
-              bl.close();
-              Navigator.push(context,
-                  MaterialPageRoute(
-                      builder: (context) =>
-                          LoginPageTwo(
-                              mobileno: mobile_no,
-                              name: Name,
-                              user_id: body['data']['pPersonUserDetails']['id'],
-                              user_credentials : true,
-                              email:body['data']['pPersonEmailDetails']['email']
-
-
-                          )));
-        }else{
-
           bl.close();
-          Navigator.push(context,
+          Navigator.push(
+              context,
               MaterialPageRoute(
-                  builder: (context) =>
-                      EmailVerfication(
-                          mobileno: mobile_no,
-                          user_credentials : true
-                      )));
+                  builder: (context) => LoginPageTwo(
+                      mobileno: mobile_no,
+                      name: Name,
+                      user_id: body['data']['pPersonUserDetails']['id'],
+                      user_credentials: true,
+                      email: body['data']['pPersonEmailDetails']['email'])));
+        } else {
+          bl.close();
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => EmailVerfication(
+                      mobileno: mobile_no, user_credentials: true)));
         }
-      }else{
+      } else {
         bl.close();
-                Navigator.push(context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            EmailVerfication(
-                              mobileno: mobile_no,
-                              user_credentials : false
-                            )));
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => EmailVerfication(
+                    mobileno: mobile_no, user_credentials: false)));
       }
-
-
     }
-
   }
 
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.only(left:25,right: 20),
+        padding: EdgeInsets.only(left: 25, right: 20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text("Ado unbox",style: TextStyle(color: Colors.orange,fontStyle: FontStyle.italic,fontSize: 60.0),),
-            Text("Version:beta",style: TextStyle(color: Colors.grey),),
-            SizedBox(height: 70.0,),
+            Text(
+              "Ado unbox",
+              style: TextStyle(
+                  color: Colors.orange,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 60.0),
+            ),
+            Text(
+              "Version:beta",
+              style: TextStyle(color: Colors.grey),
+            ),
+            SizedBox(
+              height: 70.0,
+            ),
             Container(
               child: Column(
-
                 children: <Widget>[
                   InternationalPhoneNumberInput(
-
                     onInputChanged: (PhoneNumber number) {
-
-                      if (number.phoneNumber.trim().isEmpty || number.phoneNumber.length != 13){
+                      if (number.phoneNumber.trim().isEmpty ||
+                          number.phoneNumber.length != 13) {
                         setState(() {
                           isButtonEnabled = false;
                         });
-                      }else{
+                      } else {
                         setState(() {
                           isButtonEnabled = true;
                         });
                       }
-
                     },
-
-                    onInputValidated: (bool value) {
-
-                    },
+                    onInputValidated: (bool value) {},
                     selectorConfig: SelectorConfig(
                       selectorType: PhoneInputSelectorType.BOTTOM_SHEET,
                     ),
@@ -134,8 +119,8 @@ class _LoginPageState extends State<LoginPage > {
                     initialValue: number,
                     textFieldController: phoneNo,
                     formatInput: false,
-                    keyboardType:
-                    TextInputType.numberWithOptions(signed: true, decimal: true),
+                    keyboardType: TextInputType.numberWithOptions(
+                        signed: true, decimal: true),
                     inputBorder: OutlineInputBorder(),
                     onSaved: (PhoneNumber number) {
                       print('On Saved: $number');
@@ -144,29 +129,44 @@ class _LoginPageState extends State<LoginPage > {
                 ],
               ),
             ),
-            SizedBox(height: 20,),
+            SizedBox(
+              height: 20,
+            ),
             RaisedButton(
               color: Colors.orangeAccent,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   // Icon(Icons.direc,color: Colors.white),
-                  Text('Next',style: TextStyle(color: Colors.white,),),
+                  Text(
+                    'Next',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
                 ],
               ),
-              onPressed:  isButtonEnabled?() {
-                _mobile_no_check(phoneNo.text);
-              }:null,
+              onPressed: isButtonEnabled
+                  ? () {
+                      _mobile_no_check(phoneNo.text);
+                    }
+                  : null,
             ),
-            SizedBox(height: 20,),
+            SizedBox(
+              height: 20,
+            ),
             Center(
-              child: Text("Don’t have a Login yet, Enter your Mobile Number and click Next to  Signup for a New Account",style: TextStyle(fontStyle: FontStyle.italic),),)
+              child: Text(
+                "Don’t have a Login yet, Enter your Mobile Number and click Next to  Signup for a New Account",
+                style: TextStyle(fontStyle: FontStyle.italic),
+              ),
+            )
           ],
         ),
       ),
-
     );
   }
 }
@@ -177,7 +177,14 @@ class LoginPageTwo extends StatefulWidget {
   final int user_id;
   final bool user_credentials;
   final String email;
-  const LoginPageTwo({Key key,this.mobileno,this.name,this.user_id,this.user_credentials,this.email}) : super(key: key);
+  const LoginPageTwo(
+      {Key key,
+      this.mobileno,
+      this.name,
+      this.user_id,
+      this.user_credentials,
+      this.email})
+      : super(key: key);
   @override
   _LoginPageTwoState createState() => _LoginPageTwoState();
 }
@@ -186,11 +193,11 @@ class _LoginPageTwoState extends State<LoginPageTwo> {
   final password = new TextEditingController();
   bool pwdVal = false;
   bool isButtonEnabled = false;
-  bool _isObscure = true;
+  bool _pwdDotSymbol = true;
   bool _isLoading = false;
   BottomLoader bl;
 
-  void _login() async{
+  void _login() async {
     bl = new BottomLoader(
       context,
       showLogs: true,
@@ -201,19 +208,12 @@ class _LoginPageTwoState extends State<LoginPageTwo> {
     );
     bl.display();
 
-    var data = {
-      'mobile' : widget.mobileno,
-      'password' : password.text
-    };
-
-
+    var data = {'mobile': widget.mobileno, 'password': password.text};
 
     var res = await Network().authData(data, '/wfmlogin');
     var body = json.decode(res.body);
 
-
-    if(body['status'] == 1){
-
+    if (body['status'] == 1) {
       SharedPreferences localStorage = await SharedPreferences.getInstance();
       localStorage.setString('token', json.encode(body['token']));
       localStorage.setString('user', json.encode(body['user']));
@@ -221,61 +221,68 @@ class _LoginPageTwoState extends State<LoginPageTwo> {
       bl.close();
       Navigator.push(
         context,
-        new MaterialPageRoute(
-            builder: (context) => MainPage()
-        ),
+        new MaterialPageRoute(builder: (context) => MainPage()),
       );
-    }else{
+    } else {
       bl.close();
       Navigator.push(
         context,
         new MaterialPageRoute(
             builder: (context) => WrongPassword(
-                name:widget.name,
+                name: widget.name,
                 mobileno: widget.mobileno,
                 userid: widget.user_id,
-                email:widget.email
-            )
-        ),
+                email: widget.email)),
       );
-
     }
-
-
-
   }
+
   @override
   Widget build(BuildContext context) {
     var user_name = widget.name;
     return Scaffold(
       body: Container(
-        padding: EdgeInsets.only(left:25,right: 25),
+        padding: EdgeInsets.only(left: 25, right: 25),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text("Ado unbox",style: TextStyle(color: Colors.orange,fontStyle: FontStyle.italic,fontSize: 60.0),),
-            Text("Version:beta",style: TextStyle(color: Colors.grey),),
+            Text(
+              "Ado unbox",
+              style: TextStyle(
+                  color: Colors.orange,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 60.0),
+            ),
+            Text(
+              "Version:beta",
+              style: TextStyle(color: Colors.grey),
+            ),
 
-            SizedBox(height: 70.0,),
+            SizedBox(
+              height: 70.0,
+            ),
             Align(
               alignment: Alignment.centerLeft,
-              child: Text("Hi! $user_name",style: TextStyle(fontSize: 20.0),textAlign: TextAlign.right,),),
+              child: Text(
+                "Hi! $user_name",
+                style: TextStyle(fontSize: 20.0),
+                textAlign: TextAlign.right,
+              ),
+            ),
             TextField(
-              onChanged:(val){
-
-                if (val.trim().isEmpty){
+              onChanged: (val) {
+                if (val.trim().isEmpty) {
                   setState(() {
                     isButtonEnabled = false;
                   });
-                }else{
+                } else {
                   setState(() {
                     isButtonEnabled = true;
                   });
                 }
-
               },
-              obscureText: _isObscure,
+              obscureText: _pwdDotSymbol,
               decoration: InputDecoration(
                 labelText: 'Password',
                 hintText: "Enter Password",
@@ -283,70 +290,74 @@ class _LoginPageTwoState extends State<LoginPageTwo> {
                   Icons.security,
                   color: Colors.blue,
                 ),
-                suffixIcon: isButtonEnabled?IconButton(
-                    icon: Icon(
-                        _isObscure ? Icons.visibility : Icons.visibility_off),
-                    onPressed: () {
-                      setState(() {
-                        _isObscure = !_isObscure;
-                      });
-                    }):null,
-                errorText: pwdVal ? "PassWord is required":null,
+                suffixIcon: isButtonEnabled
+                    ? IconButton(
+                        icon: Icon(_pwdDotSymbol
+                            ? Icons.visibility
+                            : Icons.visibility_off),
+                        onPressed: () {
+                          setState(() {
+                            _pwdDotSymbol = !_pwdDotSymbol;
+                          });
+                        })
+                    : null,
               ),
               controller: password,
             ),
-            SizedBox(height: 20,),
+            SizedBox(
+              height: 20,
+            ),
             RaisedButton(
               color: Colors.orangeAccent,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
               child: Row(
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
                   // Icon(Icons.login_outlined,color: Colors.white),
-                  Text(_isLoading? 'Proccessing...' : 'Login',style: TextStyle(color: Colors.white,),),
-                ],
-              ),
-              onPressed:  isButtonEnabled?() {
-                setState(() {
-                  password.text.isEmpty?pwdVal = true:pwdVal = false;
-                  if(pwdVal == false){
-                    _login();
-                  }
-                });
-
-
-              }:null,
-            ),
-            SizedBox(height: 50,),
-            InkWell(
-              onTap: (){
-
-              },
-              child: Center(
-                child: Container(
-                  padding: EdgeInsets.all(10.0),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                        color: Colors.orange,
-                        width: 1.0
-                    ),
-                    borderRadius: BorderRadius.all(
-                        Radius.circular(30.0) //                 <--- border radius here
+                  Text(
+                    'Login',
+                    style: TextStyle(
+                      color: Colors.white,
                     ),
                   ),
+                ],
+              ),
+              onPressed: isButtonEnabled
+                  ? () {
+                      _login();
+                    }
+                  : null,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            InkWell(
+              onTap: () {},
+              child: Center(
+                child: Container(
+                    padding: EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.orange, width: 1.0),
+                      borderRadius: BorderRadius.all(Radius.circular(
+                              30.0) //                 <--- border radius here
+                          ),
+                    ),
                     child: InkWell(
-                      onTap: (){
-
-                        Navigator.push(context,
-                            MaterialPageRoute(
-                                builder: (context) => distnictOTP(
-                                    mobileno:widget.mobileno,
-                                    email:widget.email
-                                )));
-
-                      },
-                        child: Text("I’m not $user_name, I don’t have a password yet Signup for a New Account",style: TextStyle(color: Colors.orange),))),),
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => distnictOTP(
+                                      mobileno: widget.mobileno,
+                                      email: widget.email)));
+                        },
+                        child: Text(
+                          "I’m not $user_name, I don’t have a password yet Signup for a New Account",
+                          style: TextStyle(color: Colors.orange),
+                        ))),
+              ),
             ),
 
             // InkWell(child:  Align(
@@ -356,7 +367,9 @@ class _LoginPageTwoState extends State<LoginPageTwo> {
             //       forgot_password(widget.user_id,widget.mobileno);
             //   },
             // ),
-            SizedBox(height: 20,),
+            SizedBox(
+              height: 20,
+            ),
             // RichText(
             //   text: TextSpan(
             //     text: "I'm not $user_name, I don't have a passoword yet'\n",
@@ -381,13 +394,4 @@ class _LoginPageTwoState extends State<LoginPageTwo> {
       ),
     );
   }
-
 }
-
-
-
-
-
-
-
-
