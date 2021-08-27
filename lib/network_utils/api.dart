80ba780766503c98a567d74e92f03f700c99bfc5
dhanaraj <dhanaraj@propelsoft>
2021-08-27 18:27:59 +0530

    Add By Common Post Codes

--- lib/network_utils/api.dart
+++ lib/network_utils/api.dart
@@ -2,10 +2,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
-class Network{
-
-
-  final String _url = 'http://localhost/wfm-main/api';
+class Network {
+  final String _url = 'http://localhost/propel-wfm-api/api';
 
   var token;
   var alldata;
@@ -15,122 +13,111 @@ class Network{
     token = jsonDecode(localStorage.getString('token'));
   }
 
-  _loadUserData() async{
+  _loadUserData() async {
     SharedPreferences localStorage = await SharedPreferences.getInstance();
-     alldata = jsonDecode(localStorage.getString('allData'));
+    alldata = jsonDecode(localStorage.getString('allData'));
   }
 
-
-  Mobile_NO_Check(apiUrl) async{
+  Mobile_NO_Check(apiUrl) async {
     var fullUrl = _url + apiUrl;
-    return await http.get(
-        fullUrl,
-        headers: _setHeaders()
-    );
+    return await http.get(fullUrl, headers: _setHeaders());
   }
 
   authData(data, apiUrl) async {
     var fullUrl = _url + apiUrl;
-    return await http.post(
-        fullUrl,
-        body: jsonEncode(data),
-        headers: _setHeaders()
-    );
+    return await http.post(fullUrl,
+        body: jsonEncode(data), headers: _setHeaders());
   }
 
   getData(apiUrl) async {
     var fullUrl = _url + apiUrl;
     await _getToken();
-    return await http.post(
-        fullUrl,
-        headers: _setHeaders()
-    );
+    return await http.post(fullUrl, headers: _setHeaders());
   }
 
   organizationsList(apiUrl) async {
     await _loadUserData();
     var personId = alldata['person_id'];
-    apiUrl+= "/$personId";
+    apiUrl += "/$personId";
     var fullUrl = _url + apiUrl;
     await _getToken();
-    return await http.get(
-        fullUrl,
-        headers: _setHeaders()
-    );
+    return await http.get(fullUrl, headers: _setHeaders());
   }
 
   categoryList(apiUrl) async {
     var fullUrl = _url + apiUrl;
 
     await _getToken();
-    return await http.get(
-        fullUrl,
-        headers: _setHeaders()
-    );
+    return await http.get(fullUrl, headers: _setHeaders());
   }
 
   categorySave(data, apiUrl) async {
     await _getToken();
     var fullUrl = _url + apiUrl;
-    return await http.post(
-        fullUrl,
-        body: jsonEncode(data),
-        headers: _setHeaders()
-    );
+    return await http.post(fullUrl,
+        body: jsonEncode(data), headers: _setHeaders());
   }
 
   categoryUpdate(data, apiUrl) async {
     await _getToken();
     var fullUrl = _url + apiUrl;
-    return await http.post(
-        fullUrl,
-        body: jsonEncode(data),
-        headers: _setHeaders()
-    );
+    return await http.post(fullUrl,
+        body: jsonEncode(data), headers: _setHeaders());
   }
 
   projectList(apiUrl) async {
     var fullUrl = _url + apiUrl;
 
     await _getToken();
-    return await http.get(
-        fullUrl,
-        headers: _setHeaders()
-    );
+    return await http.get(fullUrl, headers: _setHeaders());
   }
 
   projectCreate(apiUrl) async {
     var fullUrl = _url + apiUrl;
     await _getToken();
-    return await http.get(
-        fullUrl,
-        headers: _setHeaders()
-    );
+    return await http.get(fullUrl, headers: _setHeaders());
   }
 
   ProjectStore(data, apiUrl) async {
     await _getToken();
     var fullUrl = _url + apiUrl;
-    return await http.post(
-        fullUrl,
-        body: jsonEncode(data),
-        headers: _setHeaders()
-    );
+    return await http.post(fullUrl,
+        body: jsonEncode(data), headers: _setHeaders());
   }
 
   taskList(apiUrl) async {
     var fullUrl = _url + apiUrl;
     await _getToken();
-    return await http.get(
-        fullUrl,
-        headers: _setHeaders()
-    );
+    return await http.get(fullUrl, headers: _setHeaders());
   }
 
   _setHeaders() => {
-    'Content-type' : 'application/json',
-    'Accept' : 'application/json',
-    'Authorization' : 'Bearer $token'
-  };
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': 'Bearer $token'
+      };
 
+  postMethodWithToken(data, apiUrl) async {
+    await _getToken();
+    var fullUrl = _url + apiUrl;
+    return await http.post(fullUrl,
+        body: jsonEncode(data), headers: _setHeaders());
+  }
+
+  postMethodWithOutToken(data, apiUrl) async {
+    var fullUrl = _url + apiUrl;
+    return await http.post(fullUrl,
+        body: jsonEncode(data), headers: _setHeaders());
+  }
+
+  getMethodWithToken(apiUrl) async {
+    var fullUrl = _url + apiUrl;
+    await _getToken();
+    return await http.get(fullUrl, headers: _setHeaders());
+  }
+
+  getMethodWithOutToken(apiUrl) async {
+    var fullUrl = _url + apiUrl;
+    return await http.get(fullUrl, headers: _setHeaders());
+  }
 }
